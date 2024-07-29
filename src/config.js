a7e375a5915626b25b20c0fe5211d0c117e0a2cb
bhat315 <157919481+bhat315@users.noreply.github.com>
2024-07-30 02:06:59 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,4 +1,3 @@
-// export const BASE_URL = 'https://bookingdokter-backend.onrender.com/api/v1'
-// export const token = localStorage.getItem('token');
-export const BASE_URL='https://heath-care-backend-bhat315s-projects.vercel.app/api/v1'
+
+export const BASE_URL='https://heath-care-backend.onrender.com/api/v1'
 export const token = localStorage.getItem('token');
